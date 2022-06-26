4bb19da321bf8d54484eb71c40f284c14e5142f4
Vavelange <valentin.avelange@viacesi.fr>
2022-06-26 15:12:52 +0200

    game.py

--- Game.py
+++ Game.py
@@ -5,12 +5,17 @@ from plane import *
 class Game:
     def __init__(self, res):
         self.res = res
+
+        self.bords = [ [-30, self.res[0] + 30], [-30, self.res[1] + 30] ]
+
+        self.player_bords = [ [20, self.res[0] -20], [10, self.res[1] - 20] ]
+
         self.window_title = "WarPlane"
         self.is_running = True
         self.clock = pygame.time.Clock()
 
         self.player_plane_img = pygame.image.load("assets/warPlaneWhite.png")
-        self.missile_img = pygame.image.load("assets/missiles/missile_red.png")
+        self.missile_img = pygame.image.load("assets/missiles/missile_white.png")
 
         self.player = Plane((200, 200), 10, self.player_plane_img, self.missile_img)
 
@@ -39,7 +44,8 @@ class Game:
         if evt.type == KEYDOWN:
             if evt.key == K_SPACE:
                 missile = self.player.fire()
-                self.player_missile_group.add(missile)
+                if missile:
+                    self.player_missile_group.add(missile)
 
     def manage_pressed_keys(self):
         pressed = pygame.key.get_pressed()
@@ -57,15 +63,38 @@ class Game:
 
         self.player.move(vector[0], -vector[1])
 
+        #x
+        if self.player.pos[0] < self.player_bords[0][0]:
+            self.player.pos = (self.player_bords[0][0], self.player.pos[1]) 
+        elif self.player.pos[0] > self.player_bords[0][1]:
+            self.player.pos = (self.player_bords[0][1], self.player.pos[1])
+
+        #y
+        if self.player.pos[1] < self.player_bords[1][0]:
+            self.player.pos = (self.player.pos[0], self.player_bords[1][0])
+        if self.player.pos[1] > self.player_bords[1][1]:
+            self.player.pos = (self.player.pos[0], self.player_bords[1][1])
+
     def draw(self):
         self.screen.blit(self.player.image, self.player.rect)
         self.player_missile_group.draw(self.screen)
 
+    def clear_missile(self):
+        for missile in self.player_missile_group.sprites():
+            if missile.rect.centerx < self.bords[0][0] or missile.rect.centerx > self.bords[0][1]:
+                self.player_missile_group.remove(missile)
+            if missile.rect.centery < self.bords[1][0] or missile.rect.centery > self.bords[1][1]:
+                self.player_missile_group.remove(missile)    
+            if missile not in self.player_missile_group.sprites():
+                print("missile deleted !")
+                del missile
+
     def update(self):
         self.screen.fill((48, 44, 52))
 
-        self.player.update()
+        self.clear_missile()
 
+        self.player.update()
         self.player_missile_group.update()
 
         self.draw()
